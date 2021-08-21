84918c6fe62b32e3f0a0462749777612a4e1b306
Bilen Haile <bilen_temesgen@yahoo.com>
2021-08-20 21:17:29 -0400

    update

--- app.js
+++ app.js
@@ -1,40 +1,97 @@
 console.log("working")
 let heads = 0;
 let tails = 0;
-let coin = document.querySelector(".coin")
+let coin = document.querySelector(".coin");
 let flipBtn = $("#flip-button");
 let resetBtn = $("#reset-button");
+let headsCoin = $('.heads');
+let tailCoin = $('.tails');
+let userInput = $("#userInput")
+let playBtn = $("#submit")
+let userChoice = ""
+let h2 = $('h2')
+h2.hide()
+let h3 = $('h3')
+h3.hide()
+
+//add event listner to the play button
+
+playBtn.on('click',event=>{
+  event.preventDefault()
+ userChoice = userInput.val()
+ h2.text(`You picked ${userChoice.toUpperCase()} you can now flip the coin!`)
+userInput.hide()
+playBtn.hide()
+h2.show()
+})
 flipBtn.on("click", ()=>{
     console.log("cliked")
+  // update Points
     const updateStats = () =>{
-        $("#heads-count").
-        text(`Heads: ${heads}`);
-        $("#tails-count").
-        text(`Tails: ${tails}`);
-    }
+        $("#heads-count").text(`HEADS: ${heads}`);
+//let user = prompt ("Enter Player's Name to Play")
+// alert("welcome " + user)       
+        $("#tails-count").text(`TAILS: ${tails}`);
+        console.log(userChoice.toLowerCase())
+        //console.log(userInput.val())
+
+        if (userChoice.toLowerCase() === 'heads'&& heads == 3){
+          h3.text('You Win!')
+          h3.show()
+        }
+         if(userChoice.toLowerCase() ==='tails'&& tails == 3){
+          h3.text('You Win!')
+          h3.show()
+        }
+     if (userChoice.toLowerCase() === 'heads'&& tails == 3){
+          h3.text('You lose!')
+          h3.show()
+   }
+    if(userChoice.toLowerCase() ==='tails'&& heads == 3){
+      h3.text('You lose!')
+      h3.show()
+   }
+    };
+    // disable flip button for 3ms
+    const disableButton = () => {
+      flipBtn.disabled = true
+      setTimeout(()=>{
+        flipBtn.disabled = false;
+      }, 3000);
+    };
+
+//* ====================================== Main
+    // gets random number
     let i = Math.floor(Math.random() * 2);
-    // coin.style.animation ="none"
+    console.log((i));
+
+    // MAIN: if i = 0 (heads) else i = 1 (tails)
     if(i % 2 === 0){
+      // HEADS
         setTimeout(()=>{
         //    coin.animate("spin-heads 3s forwards")
-           coin.style.animation = "spin-heads 3s forwards"
+          coin.style.animation = "spin-heads 3s forwards"
+
        }, 100);
-       heads++;  
-    }
-    else{
+          heads++;// updates head points
+          coin.style.animation = "none"
+    } else{
+        // TAILS
+
         setTimeout(() =>{
         //    coin.animate("spin-tails 3s forwards")
-           coin.style.animation = "spin-tails 3s forwards" 
+           coin.style.animation = "spin-tails 3s forwards"
+
         }, 100);
         tails++;
-    }
-    setTimeout(updateStats, 3000);
-const disableButton = () => {
-   flipBtn.disabled = true
-   setTimeout(()=>{
-       flipBtn.disabled = false;
-   }, 3000);
-}
+        coin.style.animation = "none"
+    };
+
+// calls update stats func after 3ms
+setTimeout(updateStats, 3000);
+disableButton()
+
+// Reset button event listener
 resetBtn.on("click", ()=>{
    coin.animate = "none";
    heads = 0;
